31f7065e5e5a340bb25692092cd9f59f8cf3d547
JB Rayansh <cooldudejbrayansh@gmail.com>
2021-01-29 16:16:13 +0530

    Updates

---
+++ precache-manifest.7a9440175a37a3fa53d9afcca0e628b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f877a31a912f280e048b700472264cf",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "bae6eb8980ad4d0591e9",
+    "url": "/ReactPortfolio/static/css/main.df802349.chunk.css"
+  },
+  {
+    "revision": "45472fc5f5908f9b40de",
+    "url": "/ReactPortfolio/static/js/2.ea5dea80.chunk.js"
+  },
+  {
+    "revision": "c80ac9a9381e852bfe3f140fb667e2e6",
+    "url": "/ReactPortfolio/static/js/2.ea5dea80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae6eb8980ad4d0591e9",
+    "url": "/ReactPortfolio/static/js/main.99de8028.chunk.js"
+  },
+  {
+    "revision": "c441df768ff01e0fc807",
+    "url": "/ReactPortfolio/static/js/runtime-main.43820d5b.js"
+  }
+]);
