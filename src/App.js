f25a42771037c5f2b8b05124215f6b2db7f08f78
JB Rayansh <cooldudejbrayansh@gmail.com>
2021-01-29 16:01:47 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,78 +1,167 @@
-import React,{useState, useEffect, useRef} from "react";
-import Header from "./Header"
+import React, { useState, useEffect, useRef } from "react";
+import Header from "./Header";
 import "./App.css";
-import NET from 'vanta/dist/vanta.net.min'
-import Skills from "./Skills"
-import Projects from "./Projects"
+import NET from "vanta/dist/vanta.net.min";
+import Skills from "./Skills";
+import Projects from "./Projects";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 function App() {
-  
+  const html = "images/html.svg";
+  const react = "images/react.svg";
+  const nodejs = "images/nodejs.svg";
+  const firebase = "images/firebase2.png";
+  const mongodb = "images/mongodb4.svg";
+  const css = "images/css.svg";
+  const js = "images/js.svg";
 
-  const [vantaEffect, setVantaEffect] = useState(0)
-  const myRef = useRef(null)
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const myRef = useRef(null);
   useEffect(() => {
     if (!vantaEffect) {
-      setVantaEffect(NET({
-        el: myRef.current,
-        mouseControls: true,
-        touchControls: true,
-        gyroControls: false,
-        minHeight: 200.00,
-        minWidth: 200.00,
-        scale: 1.00,
-        scaleMobile: 1.00,
-  backgroundAlpha: 0.00
-      }))
+      setVantaEffect(
+        NET({
+          el: myRef.current,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          backgroundAlpha: 0.0,
+        })
+      );
     }
     return () => {
-      if (vantaEffect) vantaEffect.destroy()
-    }
-  }, [vantaEffect])
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
 
   return (
-   
-    <div className="app" >
+    <div className="app">
       <div>
-      <Header />
+        <Header />
       </div>
-      
+
       <div className="app__container">
-         
-          <div className="app__container__landingPage" ref={myRef}>
-             
-          </div>
-
-            <div className="app__container__about" id = "about">
-            <div>
-                <img className="app__container__about__img" src="https://scontent.fdbd1-1.fna.fbcdn.net/v/t1.0-9/82035156_1046164262400867_1890833479181008896_o.jpg?_nc_cat=100&ccb=2&_nc_sid=09cbfe&_nc_ohc=LSOZV_KLny8AX_xLy1C&_nc_ht=scontent.fdbd1-1.fna&oh=cf101f4c0b5d601688804b94fb634970&oe=60022338" alt=""/>
-            </div>
-              <div className="app__container__about__info">
-                <h3>I'm currently pursuing my B.Tech degree in Computer Science & Engineering from Rcc Institute Of Information Technology,Kolkata,WB.Currently I'm in 3rd year of my course with a ygpa of 7.4 .</h3>
-                <h3>"I'm a MERN stack web-developer and looking forward for an internship as a junior developer"</h3>
-
+        <div className="app__container__landingPage" ref={myRef}>
+          <div class="container">
+            <div class="box">
+              <div class="title">
+                <span class="block"></span>
+                <h1>
+                  I'm Mayank<span></span>
+                </h1>
               </div>
 
-            </div>
-            <div className="app__container__skills" id="skills">
-              <Skills />
-            </div>
-            <div className="app__container__projects" id="projects">
-              <h1 className="app__container__project__heading">Projects</h1>
-              <div className="all__projects__container">
-              <Projects />
-              <Projects />
-              <Projects />
-              <Projects />
-              <Projects />
-              <Projects />
+              <div class="role">
+                <div class="block"></div>
+                <p>Web developer</p>
               </div>
+            </div>
           </div>
-       </div>
-  
-        
+        </div>
+
+        <div className="app__container__about" id="about">
+          <div>
+            <img
+              className="app__container__about__img"
+              src="images/profile.jpg"
+              alt=""
+            />
+          </div>
+          <div className="app__container__about__info">
+            <h3>
+              I'm currently pursuing my B.Tech degree in Computer Science
+              Engineering from Rcc Institute Of Information
+              Technology,Kolkata,WB.Currently I'm in 3rd year of my course with
+              a ygpa of 7.4 .
+            </h3>
+            <h3>
+              "I'm a MERN stack web-developer and looking forward for an
+              internship as a junior developer"
+            </h3>
+          </div>
+        </div>
+        <section className="app__container__skills" id="skills">
+          <Skills />
+        </section>
+        <section className="app__container__projects" id="projects">
+          <h1 className="app__container__project__heading">Projects</h1>
+          <div className="all__projects__container">
+            <Projects
+              url="images/hrm2.svg"
+              title="HRM"
+              description="A Real Time human resource management website"
+              live="https://hr-management-a89a2.web.app"
+              githubF="https://github.com/asperger99/hrm-front-end"
+              githubB="https://github.com/asperger99/hrm-back-end"
+              techs={[css, js, react, nodejs, mongodb]}
+            />
+            <Projects
+              url="images/netflix.png"
+              title="Replicated Netflix"
+              description="A replicated Netflix where one can go through different genres to find out their favourite one and watch its trailer(used TMDb Api)."
+              live="https://netflix-88d7a.firebaseapp.com"
+              github="https://github.com/asperger99/Netflix-clone"
+              techs={[html, css, js, react, firebase]}
+            />
+            <Projects
+              url="images/realEstate.svg"
+              title="Real Estate"
+              description="A website for buying and selling properties."
+              live="https://ashiyana-fe39b.firebaseapp.com"
+              github="https://github.com/asperger99/RealEstate"
+              techs={[html, css, js, react, firebase]}
+            />
+            <Projects
+              url="images/crab.svg"
+              title="Crab-Crab:The Ecom-Web"
+              description="An e-commerece website."
+              live="https://frozen-river-01648.herokuapp.com/"
+              github="https://github.com/asperger99/Crab-Crab-the-Ecom-web"
+              techs={[html, css, js, nodejs, mongodb]}
+            />
+            {/* <Projects 
+                url=""
+                title=""
+                live=""
+                github=""
+              />
+              <Projects 
+                url=""
+                title=""
+                live=""
+                github=""
+              /> */}
+          </div>
+        </section>
+
+        <div>
+          <div className="app__container__footer">
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/mayank-kumar-singh-b096491b9/"
+            >
+              <LinkedInIcon className="footer__icon" />
+            </a>
+            <a target="_blank" href="https://github.com/asperger99">
+              <GitHubIcon className="footer__icon" />
+            </a>
+            <a target="_blank" href="https://www.instagram.com/__asperger__/">
+              <InstagramIcon className="footer__icon" />
+            </a>
+            <a href="https://www.facebook.com/itsRayansh/" target="_blank">
+              <FacebookIcon className="footer__icon" />
+            </a>
+          </div>
+        </div>
       </div>
-     
-   
+    </div>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,45 +1,73 @@
-import React from 'react'
-import {BrowserRouter as Router} from "react-router-dom"
-import EmailIcon from '@material-ui/icons/Email';
-import "./Header.css"
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import EmailIcon from "@material-ui/icons/Email";
+import "./Header.css";
+import { IconButton } from "@material-ui/core";
 
 function Header() {
-    return (
-        <div className="header">
-          
-        <div className="header__routers">
-      
-        <div className="header__elements">
-        <a href="#about" className="header__icon header__elements" >
+  const [change, handleChange] = useState(false);
+  const [currentSection, setCurrentSection] = useState("");
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleChange(true);
+      } else {
+        handleChange(false);
+      }
+    });
+  }, []);
+  return (
+    <div className={`navbar ${change && "navbar__black"}`}>
+      <div className="header__routers">
+        <div
+          onClick={() => {
+            setCurrentSection("about");
+          }}
+          className={`header__elements ${
+            currentSection === "about" && "active"
+          }`}
+        >
+          <a href="#about" className="header__icon header__elements">
             About
-        </a>
+          </a>
         </div>
-        
-        
-        
-        <div className="header__elements" >
-        <a href="#skills" className="header__icon header__elements">
+
+        <div
+          onClick={() => {
+            setCurrentSection("skills");
+          }}
+          className={`header__elements ${
+            currentSection === "skills" && "active"
+          }`}
+        >
+          <a href="#skills" className="header__icon header__elements">
             Skills
-        </a>
+          </a>
+        </div>
+
+        <div
+          onClick={() => {
+            setCurrentSection("projects");
+          }}
+          className={`header__elements ${
+            currentSection === "projects" && "active"
+          }`}
+        >
+          <a href="#projects" className="header__icon header__elements">
+            Projects
+          </a>
         </div>
 
         <div className="header__elements">
-        <a href="#projects" className="header__icon header__elements">
-            Projects
-        </a>
+          <a href="mailto:cooldudejbrayansh@gmail.com" target="_top">
+            <IconButton>
+              <EmailIcon className="header__icon" />
+            </IconButton>
+          </a>
         </div>
-       
-        <div className="header__elements">
-        <a href="mailto:cooldudejbrayansh@gmail.com">
-           <EmailIcon className="header__icon" />
-        </a>
-        </div>
-       
-        
-        </div>
-        
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
